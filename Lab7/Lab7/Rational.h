9f0b5829d82aadba2875a9e7b688cd739756515d
Patrik Nilsson <craftopia@live.com>
2017-03-09 21:31:28 +0100

    Last updated!

--- Lab7/Lab7/Rational.h
+++ Lab7/Lab7/Rational.h
@@ -18,6 +18,7 @@ public:
 	Rational(Rshort & rhs) : P(rhs.P), Q(rhs.Q) {}
 	Rational(Rint & rhs) : P(rhs.P), Q(rhs.Q) {}
 
+	// Print << and >>
 	friend std::ostream& operator<< (std::ostream & cout, Rational<Tint> R) { 
 		return cout << R.P << "/" << R.Q; 
 	}
@@ -30,61 +31,91 @@ public:
 		return cin;
 	}
 
-	Rational<Tint> operator-() { return Rational<Tint>(-P, Q); }
-
-	Rational<Tint>& operator++()
-	{
+	// R++ and ++R
+	Rational<Tint>& operator++(){
 		P += Q;
 		return *this;
 	}
-	Rational<Tint> operator++(int)
-	{
+	Rational<Tint> operator++(int){
 		Rational<Tint> tmp(P, Q);
 		operator++();
 		return tmp;
 	}
 
-	explicit operator int() {
-		return P / Q;
-	}
+	// R++ and ++R
+	Rational<Tint> operator-() { return Rational<Tint>(-P, Q); }
 
-
+	explicit operator int() {return P / Q;}
 };
 
-template<typename Tint, typename Tx>
-auto operator+=(const Rational<Tint>& lhs, const Tx& rhs) {
-	return Rational<Tint>(lhs.P + rhs * lhs.Q, lhs.Q);
+/*
+*		GLOBAL FUNCTIONS!
+*/
+
+// "+="
+template<typename Tint, typename Tx>									//Rational<a> --- int
+Rational<Tint> operator+=(Rational<Tint>& lhs, const Tx& rhs) {
+	lhs.P = lhs.P + rhs * (long)lhs.Q;
+	return lhs;
 }
 
-template<typename Tint, typename Tx> //Implement Selection
-auto operator+(Rational<Tint>& lhs, const Rational<Tx>& rhs) {
+template<typename Tint, typename Tx>									//Rational<a> --- Rational<b>
+Rational<Tint> & operator+=(Rational<Tint>& lhs, const Rational<Tx>& rhs) {
 	long long p = lhs.P * (long long)rhs.Q + rhs.P * (long long)lhs.Q;
 	long long q = lhs.Q * (long long)rhs.Q;
-
 	Reduce(p, q);
-	return Rational<select<Tint, Tx>::value>(p, q);
+	lhs.P = p;
+	lhs.Q = q;
+	return lhs;
 }
 
-template<typename Tint, typename Tx>
-auto operator+(Rational<Tint>& lhs, const Tx& rhs) {
+template<typename Tint>													//Rational<a> --- Rational<a>
+Rational<Tint> & operator+=(Rational<Tint>& lhs, const Rational<Tint>& rhs) {
+	long long p = lhs.P * (long long)rhs.Q + rhs.P * (long long)lhs.Q;
+	long long q = lhs.Q * (long long)rhs.Q;
+	Reduce(p, q);
+	lhs.P = p;
+	lhs.Q = q;
+	return lhs;
+}
+
+// "+" Don't use += more reduce required!
+template<typename Tint, typename Tx>									//Rational<a> --- int
+auto operator+(const Rational<Tint>& lhs, const Tx& rhs) {
 	long long p = lhs.P + rhs * (long long)lhs.Q;
 	long long q = lhs.Q;
-
 	Reduce(p, q);
 	return Rational<select<Tint, Tx>::value>(p, q);
 }
 
-template<typename Tint, typename Tx>
+template<typename Tint, typename Tx>									//int --- Rational<a>
 auto operator+(const Tint& lhs, const Rational<Tx>& rhs) {
 	long long p = rhs.P + lhs * (long long)rhs.Q;
 	long long q = rhs.Q;
-
 	Reduce(p, q);
 	return Rational<select<Tint, Tx>::value>(p, q);
 }
 
+template<typename Tint, typename Tx>									//Rational<a> --- Rational<b>
+auto operator+(const Rational<Tint>& lhs, const Rational<Tx>& rhs) {
+	long long p = lhs.P * (long long)rhs.Q + rhs.P * (long long)lhs.Q;
+	long long q = lhs.Q * (long long)rhs.Q;
+	Reduce(p, q);
+	return Rational<select<Tint, Tx>::value>(p, q);
+}
+
+template<typename Tint>													//Rational<a> --- Rational<a>
+auto operator+(const Rational<Tint>& lhs, const Rational<Tint>& rhs) {
+	long long p = lhs.P * (long long)rhs.Q + rhs.P * (long long)lhs.Q;
+	long long q = lhs.Q * (long long)rhs.Q;
+	Reduce(p, q);
+	return Rational<Tint>(p, q);
+}
+
+//Tell if Ta or Tb is largest, general
 template<class Ta, class Tb> struct select { typedef Ta value;};
 
+//Specialization
 template<> struct select<short, int> { typedef int value; };
 template<> struct select<int, short> { typedef int value; };
 template<> struct select<short, long long> { typedef long long value; };
--- Lab7/Lab7/RelOps.h
+++ Lab7/Lab7/RelOps.h
@@ -4,21 +4,17 @@ template <typename T, typename R> bool operator>(const T& lhs,const R& rhs) { re
 template <typename T, typename R> bool operator>=(const T& lhs,const R& rhs) { return !(lhs<rhs); }
 template <typename T, typename R> bool operator<=(const T& lhs,const R& rhs) { return !(rhs<lhs); }
 
-template<typename Ta> bool operator==(const Rational<Ta>& lhs, const Rational<Ta>& rhs) 
-{ return (lhs.P == rhs.P && lhs.Q == rhs.Q); }
+template<typename Ta> bool operator==(const Rational<Ta>& lhs, const Rational<Ta>& rhs)					//Rational<a> --- Rational<a>
+{ return (lhs.P == rhs.P && lhs.Q == rhs.Q); } 
 
-template<typename Ta, typename Tb> bool operator==(const Rational<Ta>& lhs, const Rational<Tb>& rhs) 
+template<typename Ta, typename Tb> bool operator==(const Rational<Ta>& lhs, const Rational<Tb>& rhs)	//Rational<a> --- Rational<b>
 { return (lhs.P / (float)lhs.Q == rhs.P / (float)rhs.Q);}
 
-template<typename Ta, typename Tb> bool operator==(const Rational<Ta>& lhs, const Tb& rhs) 
+template<typename Ta, typename Tb> bool operator==(const Rational<Ta>& lhs, const Tb& rhs)				//Rational<a> --- int
 { return (lhs.P / (float)lhs.Q == (float)rhs); }
+
+template<typename Ta, typename Tb> bool operator==(const Ta& lhs, const Rational<Tb>& rhs)				//int --- Rational<b>
+{ return (rhs.P / (float)rhs.Q == (float)lhs); }
 
 template<typename Ta, typename Tb> bool operator<(const Rational<Ta>& lhs, const Rational<Tb>& rhs) 
 { return (lhs.P / (float)lhs.Q < rhs.P / (float)rhs.Q); }
-
-
-
-template<typename Ta, typename Tb> bool operator==(const Ta& lhs, const Rational<Tb>& rhs)
-{ 
-	return (rhs.P / (float)rhs.Q == (float)lhs);
-}
